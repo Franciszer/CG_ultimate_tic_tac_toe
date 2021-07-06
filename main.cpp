fd154a82e958377c9ad7b5f08a6686b8f117bf9c
François Thierrée <francisco@kbn.home>
2021-07-07 01:32:05 +0200

    implemented win checker and working on gennerating possible moves

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 #include <vector>
 #include <algorithm>
 
-#define ME 0
-#define OPP 1
+#define CR 0
+#define CL 1
 
-#define BOARD_SZ 9 // board BOARD_SZ
+#define BOARD_SZ 9 // size of board
+#define SQ_SZ 3 // size of a square
 
 using namespace std;
 using std::ostream;
@@ -19,14 +20,14 @@ using std::ostream;
 
 struct State
 {
-	State(bool player = ME):
+	State(bool player = CR):
 	_boards{0, 0}, _wins{0, 0},
 	_player(player)
 	{};
 
 	State(const State& src):
-	_boards{src._boards[ME], src._boards[OPP]},
-	_wins{src._wins[ME], src._wins[OPP]},	
+	_boards{src._boards[CR], src._boards[CL]},
+	_wins{src._wins[CR], src._wins[CL]},	
 	_player(src._player)
 	{}; 
 
@@ -47,16 +48,76 @@ struct State
 	void	set_marking(int x, int y) { // sets current player's marking on (x, y) position
 		_boards[_player] = (_boards[_player] | ((__uint128_t)1 << (x * BOARD_SZ + y)));
 	}
+
 	bool	is_marking(bool player, int x, int y) { // return true if player's (x, y) position is marked and false otherwise
 		return (_boards[player] & ((__uint128_t)1 << (x * BOARD_SZ + y)));
 	}
+
+	bool	sq_is_win(int x, int y) { // returns true if _player wins square
+		// set the position at the top right hand corner of the square
+		x /= SQ_SZ;
+		y /= SQ_SZ;
+
+		// horizontal check
+		for (int i = 0 ; i < SQ_SZ ; i++) {
+			for (int j = 0 ; j < SQ_SZ ; j++) {
+				cout << "pos " << x + i << ' ' << y + j << endl;
+				if (!is_marking(_player, x + i, y + j))
+					break;
+				else if (j == 2)
+					return true;
+			}
+		}
+
+		// vertical check
+		for (int i = 0 ; i < SQ_SZ ; i++) {
+			for (int j = 0 ; j < SQ_SZ ; j++) {
+				if (!is_marking(_player, x + j, y + i))
+					break;
+				else if (j == 2)
+					return true;
+			}
+		}
+		return false;
+	}
+
+	// get the possible moves following current state
+	// x and y are the coordiates of the last move of the opposing player
+	// does not work if the opponent hasn't played before
+	// each set bit corresponds to a possible position
+	__uint128_t			get_possible_moves(int x, int y) {
+		__uint128_t	possible_moves = 0;
+		
+		// get the current square
+		x /= SQ_SZ;
+		y /= SQ_SZ;
+
+		// if the square is already won by either player, return all empty cells on unfinished squares
+		if (_wins[CR] & ((__uint128_t)1 << (x * SQ_SZ + y)) ||\
+			_wins[CL] & ((__uint128_t)1 << (x * SQ_SZ + y))) {
+				for (__uint16_t i = 0 ; i < 9 ; i++) {
+					// if current square is not finished
+					if (!(_wins[CR] & ((__uint16_t)1 << i)) &&\
+						!(_wins[CL] & ((__uint16_t)1 << i))) {
+							// set x and y to the beginning of the square
+							x = i / SQ_SZ * BOARD_SZ;
+							y = i % SQ_SZ * BOARD_SZ;
+					}
+				}
+			return possible_moves;
+		}
+		return possible_moves;
+	}
+
 	friend ostream&    operator<<(ostream& os, State const &state) {
 		for (int i = 0 ; i < BOARD_SZ ; i++) {
 			for (int j = 0 ; j < BOARD_SZ ; j++) {
-				if ((state._boards[state._player] & ((__uint128_t)1 << (i * BOARD_SZ + j))))
-					os << '1';
+				if ((state._boards[CR] & ((__uint128_t)1 << (i * BOARD_SZ + j))))
+					os << 'X';
+				else if ((state._boards[CL] & ((__uint128_t)1 << (i * BOARD_SZ + j))))
+					os << 'O';
 				else
-					os << '0';
+					os << '-';
 				os << ' ';
 			}
 			if (i != BOARD_SZ - 1)
@@ -65,8 +126,8 @@ struct State
 		return os;
 	}
 
-    __uint128_t		_boards[2] = {0};	// 1 map for player's and opponent's markings
-    uint16_t		_wins[2] = {0};		// map of player's winning maps
+    __uint128_t		_boards[2];	// 1 map for player's and opponent's markings
+    __uint16_t		_wins[2];		// map of player's winning maps
 	bool			_player;			// current player
 };
 
@@ -74,8 +135,13 @@ struct State
 int main()
 {
 	State	state;
-	state.set_marking(0, 0);
+	state._player = !state._player;
+	state.set_marking(0, 1);
+	state.set_marking(1, 1);
+	state.set_marking(2, 1);
 	cout << state << endl;
+	cout << (state.sq_is_win(0, 2) ? "W " : "N ");
+	cout << endl;
     // game loop
     // while (1) {
     //     int opponentRow;
--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 
 #define MINE 0
 #define OPP 1
-#define SIZE 9 // both width and height
+#define SIZE 3 // both width and height
 
 using namespace std;
 using std::ostream;
@@ -26,6 +26,18 @@ struct State
 
 __int128_t board = {0};
 
+void    print_board() {
+    for (int i = 0 ; i < SIZE ; i++) {
+        for (int j = 0 ; j < SIZE ; j++) {
+            if ((board & ((__int128_t)1 << (i * SIZE + j))))
+                cout << '1';
+            else
+                cout << '0';
+            cout << ' ';
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
